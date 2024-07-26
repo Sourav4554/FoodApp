1894ad424dc62fc9f1cb508cf64124c4720513f5
sourav <souravkrishna358@gmail.com>
2024-07-26 15:27:33 +0530

    complete the development of header section

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home/Home'
+import Cart from './Pages/Cart/Cart'
+import Placeholder from './Pages/Placeholder/Placeholder'
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/placeorder' element={<Placeholder/>}/>
+      </Routes>
     </div>
   )
 }
---
+++ Frontend/src/Components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order Your Favourite Food Here</h2>
+            <p>Explore our food app for delicious recipes, personalized meal plans, grocery lists, and cooking tips, making your culinary journey fun and effortless every day."</p>
+            <button>ViewMenu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
