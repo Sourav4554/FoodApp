3223b75902f446842cfeccf1afbd0f411657fe53
sourav <souravkrishna358@gmail.com>
2024-07-28 11:48:51 +0530

    implement cart functionality in conteaxt api

--- Frontend/src/Components/Fooditem/Fooditem.jsx
+++ Frontend/src/Components/Fooditem/Fooditem.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useContext} from 'react'
 import { assets } from '../../assets/assets'
 import './Fooditem.css'
+import { storeContext } from '../../Context/Storecontext'
 const Fooditem = ({id,name,description,image,price}) => {
+const {cartItems ,addToCart, removeFromCart}=useContext(storeContext);
   return (
     <div className='food-item'>
     <div className="food-item-img-container">
         <img  alt="" className='food-item-image' src={image}/>
+        {!cartItems[id] 
+        ? <img src={assets.add_icon_white} alt="" className='add' onClick={()=>{addToCart(id)}}/>
+        : <div className="food-item-counter">
+             <img src={assets.remove_icon_red} alt="" onClick={()=>{removeFromCart(id)}}/>
+             <p>{cartItems[id]}</p>
+             <img src={assets.add_icon_green} alt="" onClick={()=>{addToCart(id)}}/>
+        </div>
+        
+        }
     </div>
     <div className="food-item-info">
         <div className="food-item-name-rating">
--- Frontend/src/Context/Storecontext.jsx
+++ Frontend/src/Context/Storecontext.jsx
@@ -1,9 +1,30 @@
-import React, { createContext } from 'react'
+import React, { createContext, useEffect, useState } from 'react'
 import { food_list } from '../assets/assets'
 export const storeContext=createContext(null)
 const StorecontextProvider = (props) => {
+const [cartItems,setCartItems]=useState({});
+//add to cart
+const addToCart=(itemid)=>{
+if(!cartItems[itemid]){
+setCartItems((prev)=>({...prev,[itemid]:1}))
+}else{
+setCartItems((prev)=>({...prev,[itemid]:prev[itemid]+1}))
+}
+}
+//remove from cart
+const removeFromCart=(itemid)=>{
+setCartItems((prev)=>({...prev,[itemid]:prev[itemid]-1}))
+}
+useEffect(()=>{
+console.log(cartItems);
+},[cartItems])
+
+
   const contextValue={
-  food_list
+  food_list,
+  cartItems,
+  addToCart,
+  removeFromCart,
   }
   return (
     <storeContext.Provider value={contextValue}>
