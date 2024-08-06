88de06eab64f326ee5e37cdc4339fb2926db6eff
sourav <souravkrishna358@gmail.com>
2024-08-06 19:58:35 +0530

    connect with database

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import { connectDB } from './config/db.js'
 
 
 //config
@@ -10,6 +11,9 @@ const PORT=4000
 app.use(express.json())
 app.use(cors())
 
+//database connection
+connectDB();
+
 app.get('/',(req,res)=>{
 res.send('API WORKING')
 })
@@ -17,3 +21,4 @@ res.send('API WORKING')
 app.listen(PORT,()=>{
 console.log(`Server is start http://localhost:${PORT}`);
 })
+//mongodb+srv://souravkrishna:sourav425933@cluster0.gwd9puz.mongodb.net/?
