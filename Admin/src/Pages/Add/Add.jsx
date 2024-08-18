38661b5eded8a31ca123729916bd6c2636bc85e6
sourav <souravkrishna358@gmail.com>
2024-08-18 08:25:32 +0530

    sucessfully completed the list and remove food item feature in admin panel

--- Admin/src/Pages/Add/Add.jsx
+++ Admin/src/Pages/Add/Add.jsx
@@ -5,7 +5,7 @@ import axios from 'axios'
 import './Add.css'
 import { toast } from 'react-toastify';
 const Add = () => {
- const url='http://localhost:4000'
+ const url='http://localhost:4000';
 const [image,setImage]=useState(false); //for image preview
 const [data,setData]=useState({
 name:"",
--- Admin/src/Pages/List/List.jsx
+++ Admin/src/Pages/List/List.jsx
@@ -1,8 +1,60 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { toast } from 'react-toastify'
+import axios from 'axios'
+import './List.css'
 const List = () => {
+const url='http://localhost:4000'
+const [list,setList]=useState([]);
+console.log(list);
+const fethList= async()=>{
+const response= await axios.get(`${url}/api/food/list`)
+console.log(response.data);
+if(response.data.sucess){
+setList(response.data.message);
+}else{
+  toast.error('error')
+}
+}
+const removeFood=async(itemid)=>{
+const response=await axios.post(`${url}/api/food/remove`,{id:itemid});
+await fethList();
+if(response.data.sucess){
+toast.success(response.data.message)
+}
+else{
+toast.error('error')
+}
+}
+
+useEffect(()=>{
+fethList();
+},[])
   return (
-    <div>List</div>
+    <div className='list add flex-col'>
+      <p>All Foods List</p>
+      <div className="lst-table">
+        <div className="list-table-format title">
+        <b>Image</b>
+        <b>Name</b>
+        <b>Category</b>
+        <b>Price</b>
+        <b>Action</b>
+        </div>
+       {list.map((item,index)=>{
+       console.log(item.name);
+        return(
+          <div className="list-table-format" key={index}>
+            <img src={`${url}/images/`+item.image} alt="" />
+            <p>{item.name}</p>
+            <p>{item.category}</p>
+            <p>{item.price}</p>
+            <p className='cursor' onClick={()=>{removeFood(item._id)}}>X</p>
+          </div>
+          )
+        
+        })} 
+      </div>
+    </div>
   )
 }
 
