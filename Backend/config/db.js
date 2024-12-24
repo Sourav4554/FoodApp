6868b7154e80d539fe6031cdecdedded097a374c
Sourav4554 <142560714+Sourav4554@users.noreply.github.com>
2024-12-24 10:41:42 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,5 +2,6 @@ import mongoose from "mongoose";
 
 //database connection
 export const connectDB= async ()=>{
-await mongoose.connect(process.env.MONGO_URI).then(()=>{console.log('Db connected');}).catch((error)=>{console.log('error',error);})
+await mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true,
+  }).then(()=>{console.log('Db connected');}).catch((error)=>{console.log('error',error);})
 }
