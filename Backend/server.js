fb943fc44932e98f47b693470d7f06d280096dd7
Sourav4554 <142560714+Sourav4554@users.noreply.github.com>
2024-12-24 11:01:05 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,11 @@ const PORT=process.env.PORT || 4000
 
 //middlewares
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: "https://foodapp-frontend-obvc.onrender.com/", // Replace with your frontend URL
+  methods: ["GET", "POST"],
+  credentials: true,
+}));
 
 //database connection
 connectDB();
